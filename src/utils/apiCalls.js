f68434a2717b5dcbaa67fb14fadb2a509a8a6818
Promise Johnson <chiemelapromise30@gmail.com>
2022-07-15 08:59:34 +0100

    Update for deploy

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -7,7 +7,7 @@ const BASE_URL = process.env.REACT_APP_BACKEND_URL
 // const BASE_URL = 'http://localhost:5000'
 export const makeRequest = axios.create({
     baseURL: BASE_URL,
-    withCredentials: true,
+    // withCredentials: true,
     headers: {
         "Content-Type": "application/json",
         Authorization: TOKEN
